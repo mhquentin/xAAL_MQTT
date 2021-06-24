25921444d40b78c7c26909d231728e1313a67c9d
Mahé <mahe.qu56@gmail.com>
2021-06-24 11:12:25 +0200

    update

--- xaal/mqtt/__main__.py
+++ xaal/mqtt/__main__.py
@@ -1,4 +1,5 @@
 from . import xaal_mqtt
 from xaal.lib import helpers
 
-helpers.run_package(xaal_mqtt.PACKAGE_NAME,xaal_mqtt.main)
+#helpers.run_package(xaal_mqtt.PACKAGE_NAME,xaal_mqtt.main)
+xaal_mqtt.main() #le temps de la correction eng 
